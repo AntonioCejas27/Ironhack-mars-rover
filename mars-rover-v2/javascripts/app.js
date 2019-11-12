d4822b2d16547bc61b0e6e57aad1829b10325011
Antonio Cejas <22072@columbushs.com>
2019-11-12 14:45:21 -0500

    push to

--- mars-rover-v2/javascripts/app.js
+++ mars-rover-v2/javascripts/app.js
@@ -1,12 +1,40 @@
 // Rover Object Goes Here
 // ======================
-
+let Rover ={
+  Direction: "N",
+}
 // ======================
 function turnLeft(rover){
-  console.log("turnLeft was called!");
+  if(rover.direction == 'N'){
+    rover.direction = 'W'
+  }
+  else if(rover.direction == 'W'){
+    rover.direction = 'S'
+  }
+  else if(rover.direction == 'S'){
+    rover.direction = 'E'
+
+  }
+  else if(rover.direction == 'E'){
+    rover.direction = 'N'
+
+  }
 }
 
 function turnRight(rover){
+  if(rover.direction == 'N'){
+    rover.direction = 'E'
+  }
+  else if(rover.direction == 'E'){
+    rover.direction = 'S'
+  }
+  else if(rover.direction == 'S'){
+    rover.direction = 'W'
+  
+  }
+  else if(rover.direction == 'W'){
+    rover.directions = 'N'
+  }
   console.log("turnRight was called!");
 }
 
